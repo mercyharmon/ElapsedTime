e4d481122d7e63ea4d18ab02de191c61fcac7953
mercyharmon <ei7389@wayne.edu>
2018-05-08 10:38:06 -0400

    ElapsedTime

--- ElapsedTime/main.cpp
+++ ElapsedTime/main.cpp
@@ -1,15 +1,44 @@
+//==========================================================
 //
-//  main.cpp
-//  ElapsedTime
+// Title: ElapsedTime
+// Author: Mercyllia Harmon
+// Description:
+// Caculating ElapsedTime
 //
-//  Created by Mercyllia on 5/8/18.
-//  Copyright © 2018 MercylliaHarmon. All rights reserved.
-//
+//==========================================================
+#include <conio.h> // For function getch()
+#include <cstdlib> // For several general-purpose functions
+#include <fstream> // For file handling
+#include <iomanip> // For formatted output
+#include <iostream> // For cin, cout, and system
+#include <string> // For string data type
+using namespace std; // So "std::cout" may be abbreviated to "cout", for example.
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+int main()
+{
+    //Declare variables
+    int time;
+    int hour, min, sec;
+    
+    // Show application header
+    cout << " Welcome to my Application!" << endl;
+    cout << "___________________________" << endl;
+    
+    // Application processing here
+    cout << "Elapsed time in seconds assigned here: " << endl;
+    cin >> time;
+    hour = time / 3600;
+    min = time % 3600 / 60; // the modulus operator (%) computes the remainder that results from
+    
+    // performing integer divison;
+    sec = time % 60;
+    
+    // Show elapsed time in hours, minutes, and seconds
+    cout << " Elapsed time is: " << hour << ";" << min << ":" << sec << "." << endl;\
+    
+    // Show application close
+    cout << "\nEnd of my Application" << endl << endl;
+    
+    // Pause before application window closes
+    cout << "Press any key to exit ..." << endl;
+    _getch();
